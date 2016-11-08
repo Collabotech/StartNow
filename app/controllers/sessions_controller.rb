fc8849599937eaef7ef803d071c6fefdf7a152a7
ttu <ttu@sfu.ca>
2016-11-07 17:56:59 -0800

    add create in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: Params[session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      render 'new'
+    end
+  end
 end
