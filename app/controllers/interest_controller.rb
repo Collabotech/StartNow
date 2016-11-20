6880626d0729e164e137f5e7e75f65941d9b4761
Dat Tu <hoangtu_1232001@yahoo.com>
2016-11-20 02:46:05 +0000

    search box

--- app/controllers/interest_controller.rb
+++
@@ -1,2 +0,0 @@
-class InterestController < ApplicationController
-end
--- test/controllers/interest_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class InterestControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
