4bb08ced1d78c4c45242187db964526fa4128b84
Heidi Tong <heiditong888@gmail.com>
2016-11-20 01:37:32 +0000

    stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
     has_secure_password 
     validates :password, :length => {:minimum => 6 }
     validates :password_confirmation, :presence => true, :if => '!password.nil?'
+    
+    validates :email,         :presence => true, :uniqueness => true
 
 end
