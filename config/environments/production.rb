9859bdcffb5632b0f6bc7ac553e240cac0de9071
realgio95 <realgio95@hotmail.com>
2016-11-22 10:47:10 -0800

    prod.rb edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  #config.serve_static_assets = true
+  #onfig.serve_static_assets = true
 
 
   config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
@@ -97,6 +97,4 @@ Rails.application.configure do
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
-  
-
 end
