f9bd72a259a2c7cfa463e2d978074181b3544a25
realgio95 <realgio95@hotmail.com>
2016-11-22 12:11:03 -0800

    heroku confirmation thru email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,9 +92,9 @@ Rails.application.configure do
     :address              => 'smtp.sendgrid.net',
     :port                 => '587',
     :domain               => 'heroku.com',
-    :user_name            => "[username]",
-    :password             => "[password]",
-    :authentication       => :login,
+    :user_name            => ENV['USERNAME'],
+    :password             => ENV['PASSWORD'],
+    :authentication       => "plain",
     :enable_starttls_auto => true
   }
 end
