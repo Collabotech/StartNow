d0f796a15a37afe217d824291139f49ce5f8ca42
realgio95 <realgio95@hotmail.com>
2016-11-22 09:34:00 -0800

    TA see here heroku hopefully works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => 'arcane-shelf-86139.heroku.com' }
   # Disable automatic flushing of the log to improve performance.config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
