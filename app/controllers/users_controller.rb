4a586a468b368a0761ebe4c1a2db30baa619a79f
realgio95 <realgio95@hotmail.com>
2016-11-28 11:11:36 -0800

    Added instructions to user controller page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,3 +36,19 @@ class UsersController < ApplicationController
   end
 
 end
+
+# Now able to identify administrators:
+#
+# if current_user.admin?
+#   # do something
+# end
+# If the page could potentially not have a current_user set then:
+#
+# if current_user.try(:admin?)
+#   # do something
+# end
+# With the above way if current_user were nil, then it would still work without raising an undefined method admin? for nil:NilClass exception.
+#
+# The code below can be used to grant admin status to the current user.
+#
+# current_user.update_attribute :admin, true
