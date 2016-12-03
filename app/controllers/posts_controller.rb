26d044212d4562b8384f85cf952d6165aa5dd8a7
Manpreet Kaur <mka111@sfu.ca>
2016-12-03 00:23:00 +0000

    Upvote Link

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,11 +20,13 @@ class PostsController < ApplicationController
  def upvote
     @post = Post.find(params[:id])
     @post.upvote_by current_user
+    redirect_to posts_path
   end
   
   def downvote
    @post = Post.find(params[:id])
     @post.downvote_by current_user
+    redirect_to @posts
   end
 
  # def index(set)
