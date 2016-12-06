9893b9002f392a9bb91ab1229b922b0c39b38935
realgio95 <realgio95@hotmail.com>
2016-12-05 22:47:02 -0800

    Need to add admin view to see list of users

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "mailbox/sent" => "mailbox#sent", as: :mailbox_sent
 
   get "mailbox/trash"  => "mailbox#trash", as: :mailbox_trash
-  
+
   # conversations
   resources :conversations do
     member do
@@ -14,10 +14,26 @@ Rails.application.routes.draw do
       post :untrash
     end
   end
+
+  # Now able to identify administrators:
+  #
+  # if current_user.admin?
+  #   # do something
+  # end
+  # If the page could potentially not have a current_user set then:
+  #
+  # if current_user.try(:admin?)
+  #   # do something
+  # end
+  # With the above way if current_user were nil, then it would still work without raising an undefined method admin? for nil:NilClass exception.
+  #
+  # The code below can be used to grant admin status to the current user.
+  #
+  # current_user.update_attribute :admin, true
   
   get '/users/index' => 'users#index'
-  
-  
+
+
 
   devise_for :users, :path => 'accounts'
 
