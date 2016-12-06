7eb81581d79911b123b7c7cdabdacb8c3be5f23e
realgio95 <realgio95@hotmail.com>
2016-12-05 22:22:01 -0800

    Added the columns using migrations for city, lat and lon

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205083805) do
+ActiveRecord::Schema.define(version: 20161206060513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,6 +125,9 @@ ActiveRecord::Schema.define(version: 20161205083805) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.boolean  "admin",                  default: false
+    t.string   "city"
+    t.float    "longitude"
+    t.float    "latitude"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.create(
+    :name => "Admin",
+    :email => "startnow.collabotech@gmail.com",
+    :password => "startword",
+    :password_confirmation => "startword"
+)
+admin.toggle!(:admin)
